23d7c0ce871956a4656fdcd6d24b1f8a598b3f34
DamperGerm921 <granton8ter05@gmail.com>
2025-04-09 19:17:32 -0400

    4/9/25

---
+++ Comparison Operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x = 10;
+    double y = 5;
+    bool result = x == y;
+    cout << boolalpha << result << endl;
+
+    //If I were to compare letters, it would be CASE SENSITIVE
+    // As with all things in C++
+
+    return 0;
+}
---
+++ Logical Operators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age = 10;
+    int salary = 50'000;
+    bool isEligible = (age > 18 && age < 65)  (salary > 30'000);
+    cout << boolalpha << isEligible;
+
+}
