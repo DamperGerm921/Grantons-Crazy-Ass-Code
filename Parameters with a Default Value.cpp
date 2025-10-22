5b565c1500553e91ef92ea41dfe208d7412b6b2b
DamperGerm921 <granton8ter05@gmail.com>
2025-10-21 22:53:38 -0400

    Add files via upload

---
+++ Parameters with a Default Value.cpp
@@ -0,0 +1,14 @@
+ #include <iostream>
+
+using namespace std;
+
+double calculateTax(double income, double expense, double taxRate = .2) {
+ return income * taxRate;
+}
+
+int main() {
+double tax = calculateTax(10'000, .3);
+ cout << tax;
+
+ return 0;
+}
