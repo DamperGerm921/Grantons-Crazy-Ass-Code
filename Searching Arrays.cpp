df6c6e4a76f1ce86c0640eee5380ffdf41594cf9
DamperGerm921 <granton8ter05@gmail.com>
2026-06-28 22:48:43 -0400

    Add files via upload

---
+++ Searching Arrays.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int find(int numbers[], int size, int target) {
+    for (int i = 0; i < size; i++)
+        if (numbers[i] == target)
+            return i;
+    return -1;
+}
+
+int main() {
+    int numbers[] = { 10, 20, 30};
+    cout << find(numbers, size(numbers), 10);
+
+
+    return 0;
+}
